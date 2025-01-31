af0b9a44843a4918ebc7c1646d3d64b0ecb3c52a
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-01-31 18:17:05 -0500

    Time: 2 ms (82.14%), Space: 51.1 MB (46.69%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -1,32 +1,15 @@
 var lastStoneWeight = function(stones) {
     while(stones.length > 1) {
-        let x = -1; // Index of largest
-        let y = -1; // Index of second largest
+        // Sort in descending order
+        stones.sort((a, b) => b - a);
         
-        // Find two heaviest stones
-        for(let i = 0; i < stones.length; i++) {
-            if(x === -1 || stones[i] > stones[x]) {
-                y = x;
-                x = i;
-            } else if(y === -1 || stones[i] > stones[y]) {
-                y = i;
-            }
-        }
+        // Get and remove first two stones
+        let x = stones.shift();
+        let y = stones.shift();
         
-        // Calculate new weight
-        let newWeight = stones[x] - stones[y];
-        
-        // Remove the two stones and add new weight if necessary
-        if(x > y) {
-            stones.splice(x, 1);
-            stones.splice(y, 1);
-        } else {
-            stones.splice(y, 1);
-            stones.splice(x, 1);
-        }
-        
-        if(newWeight > 0) {
-            stones.push(newWeight);
+        // If different weights, push the difference
+        if(x !== y) {
+            stones.push(x - y);
         }
     }
     
