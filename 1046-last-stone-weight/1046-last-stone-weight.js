7cd14da2d53eaf0ffde801a5006f6652926a2462
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-01-31 20:11:17 -0500

    Time: 0 ms (100%), Space: 50.4 MB (81.19%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -1,12 +1,15 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
 var lastStoneWeight = function(stones) {
     while(stones.length>1){
-        stones.sort((a,b)=>b-a)
-        let largest = stones.shift()
-        let next_largest = stones.shift()
-
-        if(largest!=next_largest){
-            stones.push(largest-next_largest)
-        }
+    stones.sort((a,b)=>b-a)
+    let largest = stones.shift()
+    let next_largest = stones.shift()
+    if(largest!=next_largest){
+        stones.push(largest-next_largest)
+    }
     }
     return stones.length===0?0:stones[0]
 };
