bc7929a66536ccd84978ec5f37397390e5da3158
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-02-12 10:10:10 -0500

    Time: 3 ms (38.24%), Space: 50.1 MB (53.58%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let left = 0
+    let right = nums.length-1
+    while(left<right){
+        if(nums[left]+nums[right]==target){
+            return [left+1,right+1]
+        }
+        else if(nums[left]+nums[right]<target){
+            left++
+        }
+        else{
+            right--
+        }
+    }
+    return -1
+};
