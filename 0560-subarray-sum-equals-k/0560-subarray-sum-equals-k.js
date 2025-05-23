bbe4af939831d401bf45cafa961aaba03386183d
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-05-23 09:51:49 -0400

    Time: 18 ms (67.98%), Space: 63.7 MB (45.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -7,14 +7,13 @@ var subarraySum = function(nums, k) {
     let count = 0
     let runningSum = 0
     let hashMap = new Map()
-    hashMap.set(0,1) //TO handle the base case
-    for(let i =0;i<=nums.length-1;i++){
-        runningSum += nums[i]
+    hashMap.set(0,1)
+    for(let i =0;i<nums.length;i++){
+        runningSum += nums[i];
         if(hashMap.has(runningSum-k)){
-            count+= hashMap.get(runningSum - k)
+            count += hashMap.get(runningSum-k)
         }
-        hashMap.set(runningSum,(hashMap.get(runningSum) || 0)+1)
-
+        hashMap.set(runningSum, (hashMap.get(runningSum)||0) +1)
     }
     return count
 };
