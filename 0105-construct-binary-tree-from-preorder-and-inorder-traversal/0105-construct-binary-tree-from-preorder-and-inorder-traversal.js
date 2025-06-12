a16649a3a83bda614ad0f2cdd2fac5d1847ddbb3
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-06-12 13:20:16 -0400

    Time: 3 ms (76.18%), Space: 60.1 MB (54.19%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -14,6 +14,7 @@
 var buildTree = function(preorder, inorder) {
     let hashMap = new Map()
     for(let i=0;i<inorder.length;i++){
+        //Build a map so we can get the index in constant time
         hashMap.set(inorder[i],i)
     }
     // console.log(hashMap)
@@ -21,14 +22,20 @@ var buildTree = function(preorder, inorder) {
     let rootIndex = 0
     function build(start,end){
         if(start>end) return null
+        //PreOrder first index is always root 
         let rootVal = preorder[rootIndex]
+        //Once we have the value increment becasue next call will be root
         rootIndex+=1
+        //Create a TreeNode ele with root.val in it
         let root = new TreeNode(rootVal)
+        //Get the root in the inOrder so we can split it in 2 section
         let inOrderIndex = hashMap.get(rootVal)
+        //left section
         root.left = build(start,inOrderIndex-1)
+        //right section
         root.right = build(inOrderIndex+1,end)
+        //return root
         return root
     }
-    
     return build(0,inorder.length-1)
 };
