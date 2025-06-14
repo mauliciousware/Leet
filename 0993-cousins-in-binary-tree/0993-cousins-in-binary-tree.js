3d0da5c04848fb96aaf2fbfc4ae54e6bf1f7fe8c
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-06-14 02:50:58 -0400

    Time: 1 ms (30.06%), Space: 57.7 MB (5.78%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.js
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.js
@@ -1,63 +1,37 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} x
- * @param {number} y
- * @return {boolean}
- */
 var isCousins = function(root, x, y) {
-    if(!root) return false
+    if(!root)return false
+    if(root.val == x || root.val == y)return false
 
-    if(root.left && root.right) {
-        if((root.left.val === x && root.right.val === y) || 
-           (root.left.val === y && root.right.val === x)) {
-            return false;
-        }
-    }
-
-    let q = [root]
-    let parentQ = [null]
-
-    while(q.length){
-        let size = q.length
-            let foundX = false
-            let foundY = false
+    let queue = [[root,null]] //[child,parent] in a queue
             let parentX = null
             let parentY = null
+    while(queue.length){
+        let size = queue.length
+        parentX = null
+        parentY = null
         for(let i=0;i<size;i++){
-            let current = q.shift()
-            let currentParent = parentQ.shift()
-
-            if(current.val ==x){
-                foundX = true
-                parentX = currentParent
+            let [current,parent] = queue.shift()
+            
+            if(current.val==x){
+                parentX =parent
             }
             if(current.val ==y){
-                foundY = true
-                parentY = currentParent
+                parentY =parent
             }
 
-            if (current.left) {
-                q.push(current.left);
-                parentQ.push(current);
-            }
-            if (current.right) {
-                q.push(current.right);
-                parentQ.push(current);
-        }
-    }
-            if(foundX && foundY){
-            if(parentY != parentX) return true
-        }
 
-    
+            if(current.left){
+                //Pushing [next left,current as parent]
+                queue.push([current.left,current])
+            }
+            if(current.right){
+                queue.push([current.right,current])
+
+            }
+        }
+        if(parentX && parentY) {
+            if(parentX != parentY) return true
+        }        
     }
     return false
 };
