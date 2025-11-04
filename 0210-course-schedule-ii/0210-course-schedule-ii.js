a19ed331c428e272a02ddf18be704f4715f4a29e
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-11-04 17:16:16 -0500

    Time: 91 ms (5.03%), Space: 66.3 MB (5.25%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    let graph = new Map()
+    let count = []
+    let inDegree = new Array(numCourses).fill(0)
+    //Build Graph
+    for(let i = 0;i<numCourses;i++){
+        graph.set(i,[])
+    }
+    for(let [course,preReq] of prerequisites){
+        graph.get(preReq).push(course)
+    }
+    //map ready pre -> course
+    for(let i =0;i<numCourses;i++){
+        for(let j of graph.get(i)){
+            inDegree[j]++ 
+        }
+    }
+    let queue = []
+    for(let i =0;i<inDegree.length;i++){
+        if(inDegree[i]==0) queue.push(i)
+    }
+    while(queue.length){
+        let current = queue.shift()
+        count.push(current)
+        console.log(graph.get(current),"current in queue")
+        for(let neighbours of graph.get(current)){
+                inDegree[neighbours]--
+                if(inDegree[neighbours] ==0) queue.push(neighbours)
+    }
+}
+return count.length === numCourses ? count : []
+};
