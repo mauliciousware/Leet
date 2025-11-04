1f22901a9b98540935c49a2c42a18fd428b5dd73
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-11-04 16:55:41 -0500

    Time: 128 ms (5.07%), Space: 67.4 MB (5.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -5,35 +5,33 @@
  */
 var canFinish = function(numCourses, prerequisites) {
     let graph = new Map()
+    let count = 0
     let inDegree = new Array(numCourses).fill(0)
-
-    for(let i= 0;i<numCourses;i++){
+    //Build Graph
+    for(let i = 0;i<numCourses;i++){
         graph.set(i,[])
     }
     for(let [course,preReq] of prerequisites){
         graph.get(preReq).push(course)
-        inDegree[course]+=1
+    }
+    //map ready pre -> course
+    for(let i =0;i<numCourses;i++){
+        for(let j of graph.get(i)){
+            inDegree[j]++ 
+        }
     }
     let queue = []
-    let courseComplete = 0
-
     for(let i =0;i<inDegree.length;i++){
-        if(inDegree[i]==0){
-            queue.push(i)
-        }
+        if(inDegree[i]==0) queue.push(i)
     }
-    //We have a starting point
     while(queue.length){
-        let currentCourse = queue.shift()
-        courseComplete+=1
-        for(let neighbours of graph.get(currentCourse)){
-            //neighbours will iterate over the array
-            inDegree[neighbours]--
-            if(inDegree[neighbours] ==0){
-            queue.push(neighbours)
-        }
-        }
-        
+        let current = queue.shift()
+        count++
+        console.log(graph.get(current),"current in queue")
+        for(let neighbours of graph.get(current)){
+                inDegree[neighbours]--
+                if(inDegree[neighbours] ==0) queue.push(neighbours)
     }
-    return courseComplete === numCourses
+}
+return count === numCourses
 };
