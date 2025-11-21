c7481a6518e7249c067f00ebee8a8bb73a983595
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-11-21 18:29:45 -0500

    Time: 5 ms (58.21%), Space: 57 MB (76.54%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -2,30 +2,28 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var maxAreaOfIsland = function (grid) {
+var maxAreaOfIsland = function(grid) {
     let ROW = grid.length
     let COL = grid[0].length
-    let area = 0
-    let directions = [[1,0],[0,1],[-1,0],[0,-1]]
-    for(let i =0;i<ROW;i++){
-        for(let j=0;j<COL;j++){
+    let direction = [[1,0],[0,1],[-1,0],[0,-1]]
+    let maxArea = 0
+    for(let i= 0;i<ROW;i++){
+        for(let j= 0;j<COL;j++){
             if(grid[i][j] == 1){
-            area = Math.max(area,explore(i,j))
+                maxArea = Math.max(maxArea,explore(i,j))
             }
         }
     }
+    return maxArea
+
     function explore(i,j){
-        if(i < 0 || j < 0 || i >= ROW || j >= COL || grid[i][j] == 0 ) return 0
-        let count = 1 
-        grid[i][j] = 0 // mark visited
-        for(let [dr,dc] of directions){
-            let nr = dr + i
-            let nc = dc + j
-            count+= explore(nr,nc)
+        if(i < 0 || j< 0 || i >= ROW || j>= COL || grid[i][j] ==0 ) return 0
+        let count = 1
+        grid[i][j] = 0
+        for(let [dr,dc] of direction){
+            count+=explore(i+dr,j+dc)
         }
-        return count
+    return count
+
     }
-
-    return area
-
 };
