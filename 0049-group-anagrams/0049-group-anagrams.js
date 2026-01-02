36464bc7fb357841564dd3feaacf9e25e4ca42e8
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-02 16:44:52 -0500

    Time: 40 ms (32.4%), Space: 64.5 MB (99.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,27 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    const hashMap = new Map();
-    const result = [];
-
-    for (let i = 0; i < strs.length; i++) { // Index-based loop for strs
-        const count = new Array(26).fill(0); // Initialize a count array for 26 letters
-        const str = strs[i];
-
-        for (let j = 0; j < str.length; j++) { // Index-based loop for characters
-            const charCode = str.charCodeAt(j) - 'a'.charCodeAt(0); // Calculate character index
-            count[charCode]++; // Increment the count for the character
-        }
-
-        const key = count.join('#'); // Convert the count array to a string key
-        if (hashMap.has(key)) {
-            hashMap.get(key).push(str); // Add the original string to the corresponding group
-        } else {
-            hashMap.set(key, [str]); // Create a new group for the key with the current string
-        }
+    let hashMap = new Map()
+    let res = []
+    for(let s of strs){
+        if(hashMap.has(s.split('').sort().join(''))) hashMap.get(s.split('').sort().join('')).push(s)
+        else hashMap.set(s.split('').sort().join(''),[s])
     }
-
-    for (let key of hashMap.keys()) {
-        result.push(hashMap.get(key)); // Push each group into the result array
+    //hashmap ready
+    for(let [key,value] of hashMap){
+        res.push(value)
     }
-
-    return result;
+    return res
 };
