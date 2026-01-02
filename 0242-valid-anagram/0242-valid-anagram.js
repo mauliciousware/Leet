5e3809dece7e88c93453440258dc5a43b276b839
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-02 13:16:44 -0500

    Time: 4 ms (98.2%), Space: 54.1 MB (99.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,16 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('')
+    if(s.length != t.length) return false
+    let charArray = new Array(26).fill(0)
+    for(let i=0;i<s.length;i++){
+        charArray[s.charCodeAt(i)-"a".charCodeAt(0)]++
+        charArray[t.charCodeAt(i)-"a".charCodeAt(0)]--
+    }
+
+    for(let i=0;i<charArray.length;i++){
+        if(charArray[i]!==0) return false
+    }
+
+    return true
 };
