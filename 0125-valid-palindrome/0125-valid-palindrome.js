bf7958d1f064932afe05d6c83c6647bf43c1d8ea
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-05 13:00:56 -0500

    Time: 2 ms (92.84%), Space: 56.4 MB (88.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,28 +1,22 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isPalindrome = function(s) {
-    s= s.toLowerCase()
-    function isAlpha(char){
-        return((char>="a"&&char<="z") || (char>="0"&&char<="9"))
+    s = s.toLowerCase()
+    function isValidChar(sts){
+        return (sts>="0" && sts<="9" || sts>="a" && sts<="z")
     }
-
     let left = 0
-    let right = s.length -1
-    while(left<right){
-        while(left<right && !isAlpha(s[left])){
+    let right = s.length-1
+
+    while(left < right){
+        while( left < right && !isValidChar(s[left])){
             left++
         }
-        while(left<right && !isAlpha(s[right]))
-        {
+        while(left < right && !isValidChar(s[right])){
             right--
-        }
-        if(s[left]!=s[right]){
-            return false
-        }
+        }        
+    
+        if(s[left]!==s[right]) return false
         left++
         right--
     }
-    return true
+    return true 
 };
