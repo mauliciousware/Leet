75043d45eb70596f7459765f911dfe91c024d022
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-05 14:34:34 -0500

    Time: 0 ms (100%), Space: 63.4 MB (56.46%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -6,7 +6,7 @@ var maxArea = function(height) {
     let res = -Infinity
     let left = 0 
     let right = height.length-1
-    while(left < right){
+    while(left <= right){
         res = Math.max(res,(Math.min(height[left],height[right]) * (right-left)))
         if(height[left]<= height[right]) left++
         else if(height[left] > height[right]) right--
