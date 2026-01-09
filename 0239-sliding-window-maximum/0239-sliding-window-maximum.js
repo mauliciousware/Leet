715bd83057fb9dd8d69ab0c110155e985fb6ece4
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-09 16:14:38 -0500

    Time: 26 ms (96.36%), Space: 89.6 MB (8.29%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -1,30 +1,34 @@
 var maxSlidingWindow = function(nums, k) {
 
     // https://codepen.io/mrmaddy7/pen/LEZZPva
-
     
-    // Deque will store indices of elements
+    let deque = []; // Deque will store indices of elements
     // Values corresponding to these indices will be in decreasing order
-    let deque = [];
-    let result = [];
+    let result = []; // This will store result
 
     for (let i = 0; i < nums.length; i++) {
 
         // Explicitly compute the start of the window
-        let windowStart = i - k + 1;
+        let windowStart = i - k + 1; // This basically tells, which part of window will i fall on 
 
         //Remove indices that are out of the current window
         // Current window range is [i - k + 1, i]
         if (deque.length > 0 && deque[0] < windowStart) {
+            //this basically tells anything below dequeue[0](which is holding indices should be removed)
+            //dequeue[0] is passed my window
+            //this keep my window size intact
+            //Window size controlling popFront()
             deque.shift();
         }
 
         //Remove all smaller elements from the back
         // They can never be the maximum if current element is bigger
         while (deque.length > 0) {
+            //Keep comparing value of last index of dequeu
             let lastIndex = deque[deque.length - 1];
 
             if (nums[lastIndex] <= nums[i]) {
+                //value of the nums at last index controls popBack()
                 deque.pop();
             } else {
                 break;
