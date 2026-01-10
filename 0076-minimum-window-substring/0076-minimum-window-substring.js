d2e475ba555b1ebcc66ccd8ca2762e4cfea5eb38
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-10 17:32:30 -0500

    Time: 24 ms (64.76%), Space: 55.3 MB (99.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -1,36 +1,44 @@
 var minWindow = function(s, t) {
     let tMap = new Map();
-    for (let c of t) {
-        tMap.set(c, (tMap.get(c) || 0) + 1);
-    }
-
-    let required = tMap.size;
     let matches = 0;
-    let minLength = Infinity;
-    let start = 0;
 
-    let i = 0, j = 0;
+    for (let i = 0; i < t.length; i++) {
+        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    }
+    //T map is ready above
+
+    let i = 0;
+    let j = 0;
+    let miniLength = Infinity;
+    let startingIndex = 0;
 
     while (i < s.length) {
         if (tMap.has(s[i])) {
             tMap.set(s[i], tMap.get(s[i]) - 1);
-            if (tMap.get(s[i]) === 0) matches++;
-        }
 
-        while (matches === required) {
-            if (i - j + 1 < minLength) {
-                minLength = i - j + 1;
-                start = j;
+            if (tMap.get(s[i]) === 0) {
+                matches++;
+            }
+        }
+        //i will move ahead when dont have all matches
+        while (matches === tMap.size) {
+            if (i - j + 1 < miniLength) {
+                miniLength = i - j + 1;
+                startingIndex = j;
             }
 
             if (tMap.has(s[j])) {
                 tMap.set(s[j], tMap.get(s[j]) + 1);
-                if (tMap.get(s[j]) > 0) matches--;
+                if (tMap.get(s[j]) > 0) {
+                    matches--;
+                }
             }
             j++;
         }
         i++;
     }
 
-    return minLength === Infinity ? "" : s.substring(start, start + minLength);
+    return miniLength === Infinity
+        ? ""
+        : s.substring(startingIndex, startingIndex + miniLength);
 };
