0417dac6309dc43aed2db959f445d79767326c20
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-01-10 19:12:40 -0500

    Time: 130 ms (55.27%), Space: 64.3 MB (60.94%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -5,31 +5,29 @@
  * @return {number}
  */
 var ladderLength = function(beginWord, endWord, wordList) {
-    const wordSet = new Set(wordList);
-    if (!wordSet.has(endWord)) return 0;
+    let wordSet = new Set(wordList)
+    if(!wordSet.has(endWord)) return 0 
 
-    let queue = [[beginWord, 1]]; // word, transformation length
+    let queue = [[beginWord,1]]
 
-    while (queue.length) {
-        const [word, level] = queue.shift();
+    while(queue.length){
+        let [word,level] = queue.shift()
 
-        if (word === endWord) return level;
+        if(word == endWord) return level
 
-        for (let i = 0; i < word.length; i++) {
-            for (let c = 97; c <= 122; c++) { // 'a' to 'z'
-                const char = String.fromCharCode(c);
-                // if (char === word[i]) continue; //dont process duplicates
+        for(let i =0;i<word.length;i++){
+            for(let j=97; j<= 122;j++){
+                let char = String.fromCharCode(j)
 
-                const newWord =
-                    word.slice(0, i) + char + word.slice(i + 1);
-
-                if (wordSet.has(newWord)) {
-                    queue.push([newWord, level + 1]);
-                    wordSet.delete(newWord); // mark visited
+                let before = word.slice(0,i)
+                let after = word.slice(i+1)
+                let newWord = before + char + after
+                if(wordSet.has(newWord)){
+                    queue.push([newWord,level +1])
+                    wordSet.delete(newWord) // mark visited
                 }
             }
         }
     }
-
-    return 0;
+    return 0
 };
