59e9f42025a94b33bf6662884d969e483860408c
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-04-30 22:40:25 -0400

    Time: 1 ms (88.9%), Space: 63.3 MB (87.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -9,28 +9,36 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    if(!head || !head.next) return head
-    //LinkedList To Array
-    let ll_array = []
-    let current = head
+function reverseList(current){
+    prev = null
     while(current){
-        ll_array.push(current)
-        current = current.next
+        let front = current.next
+        current.next = prev
+        prev = current
+        current = front
     }
-    let left = 0
-    let right = ll_array.length-1
+    return prev
+}
+var reorderList = function(head) {
+      //Half Reverse the linked list
+      let slow = head
+      let fast = head
+      while(fast.next && fast.next.next){
+        slow = slow.next
+        fast = fast.next.next
+      }
+      fast=reverseList(slow.next)
+      slow.next = null
+      slow = head
+      //Slow is pointing at the first ele
+      //Fast is pointing to the last ele
+      while(fast!=null){
+        let front = slow.next
+        slow.next = fast
+        fast = fast.next
+        slow.next.next = front
+        slow = front
+      }
 
-    while(left < right){
-        ll_array[left].next = ll_array[right]
-        left++
-
-        if(left < right){
-            ll_array[right].next = ll_array[left]
-            right--
-        }
-    }
-
-    ll_array[left].next = null
-
+      // merge
 };
