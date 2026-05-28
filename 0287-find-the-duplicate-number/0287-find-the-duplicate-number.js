9eacc74e43597346e351879d6781113e12c53774
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-05-28 00:08:08 -0400

    Time: 9 ms (48.09%), Space: 63.5 MB (68.61%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    // Phase 1: Find the intersection point in the cycle
-    let slow = nums[0];
-    let fast = nums[0];
-    
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow !== fast);
-    
-    // Phase 2: Find the entrance to the cycle (the duplicate number)
-    slow = nums[0];
-    while (slow !== fast) {
-        slow = nums[slow];
-        fast = nums[fast];
+    let slow = nums[0]
+    let fast = nums[0]
+    while(true){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if(slow == fast) break
     }
-    
-    return fast;
+    slow = nums[0]
+    while(slow!=fast){
+        slow = nums[slow]
+        fast = nums[fast]
+    }
+
+    return fast
+
 };
