bd8b25b555972b6b71e4821edc3846e270ebbd27
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-09-26 09:28:36 -0400

    Time: 1 ms (96.49%), Space: 57.2 MB (98.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -14,10 +14,11 @@ var combinationSum = function(candidates, target) {
             return
         }
 
-        dfs(candidates,idx+1,target,res) // skip
+        
         res.push(candidates[idx])
         dfs(candidates,idx,target-candidates[idx],res) // choose
         res.pop()
+        dfs(candidates,idx+1,target,res) // skip
     }
     dfs(candidates,0,target,[])
     return finalRes
