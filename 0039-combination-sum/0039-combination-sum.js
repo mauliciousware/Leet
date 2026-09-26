2f1ccf91303769336e09ae0f8e3421d299cbd939
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2026-09-26 08:17:01 -0400

    Time: 11 ms (8.14%), Space: 64.2 MB (5.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -4,20 +4,20 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    let res = []
-    function helper(candidates,target,amount,path,idx){
-        if(amount == target){
-            res.push(path)
-            return 
+    let finalRes = []
+    function dfs(candidates,idx,target,res){
+        if (target < 0 || idx >= candidates.length) {
+            return
         }
-        if(idx >= candidates.length || amount>target){
-            return 
+        if(target ==0 ){
+            finalRes.push([...res])
+            return
         }
-        let newArray = [...path,candidates[idx]] // Create a DEEP COPY
-        helper(candidates,target,amount+candidates[idx],newArray,idx)
-        helper(candidates,target,amount,path,idx+1)
-        return res
+
+
+        dfs(candidates,idx+1,target,[...res]) // skip
+        dfs(candidates,idx,target-candidates[idx],[...res,candidates[idx]]) // choose
     }
-    helper(candidates,target,0,[],0)
-    return res
+    dfs(candidates,0,target,[])
+    return finalRes
 };
